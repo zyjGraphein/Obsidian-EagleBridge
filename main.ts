59e991eb8c9f44ecebe4c434e4e3f5fb48ac8226
zyjGraphein <146322722+zyjGraphein@users.noreply.github.com>
2025-01-09 23:04:31 +0800

    修改使用api直接调取，以及将普通跳转链接进行了修改

--- main.ts
+++ main.ts
@@ -20,11 +20,16 @@ export default class MyPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-
+		// 注册图片右键菜单事件
 		this.registerDocument(document);
 		this.app.workspace.on("window-open", (workspaceWindow, window) => {
 			this.registerDocument(window.document);
 		});
+		// //注册新的链接点击事件
+		// this.registerLinkClickEvent(document);
+		// this.app.workspace.on("window-open", (workspaceWindow, window) => {
+		// 	this.registerLinkClickEvent(window.document);
+		// });
 		// 在插件加载时启动服务器
 		startServer(this.settings.libraryPath, this.settings.port);
 
@@ -37,6 +42,16 @@ export default class MyPlugin extends Plugin {
 				handlePasteEvent(clipboard, editor, this.settings.port);
 			})
 		);
+
+		this.registerDomEvent(document, "click", (event: MouseEvent) => {
+			const target = event.target as HTMLElement;
+			if (target.matches("span.external-link, a.external-link")) {
+				event.preventDefault();
+				event.stopPropagation();
+				console.log('阻止默认的链接跳转行为');
+				this.handleLinkClick(event);
+			}
+		}, { capture: true });
 	}
 
 	onunload() {
@@ -51,23 +66,94 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+	// 注册图片右键菜单事件
 	registerDocument(document: Document) {
 		this.register(
 			onElement(
 				document,
 				"mousedown",
 				"img",
-				this.eagleImageContextMenuCall.bind(this)
+				this.eagleImageContextMenuCall.bind(this),
+				// { capture: true }
 			)
 		);
 	}
+	// // 注册链接点击事件
+	// registerLinkClickEvent(document: Document) {
+	// 	this.register(
+	// 		onElement(
+	// 			document,
+	// 			"click",
+	// 			"span.external-link, a.external-link",
+	// 			this.handleLinkClick.bind(this)
+	// 		)
+	// 	);
+	// }
+	// 处理链接点击事件
+	handleLinkClick(event: MouseEvent) {
+		// event.preventDefault(); // 阻止默认的跳转行为
+		// event.stopPropagation(); // 阻止事件冒泡
+		// 创建一个新的菜单实例
+		const menu = new Menu();
+
+		// 添加菜单项
+		menu.addItem((item: MenuItem) =>
+			item
+				.setIcon("link")
+				.setTitle("Open Link")
+				.onClick(() => {
+					const link = event.target as HTMLAnchorElement;
+					if (link && link.href) {
+						window.open(link.href, '_blank'); // 在新窗口中打开链接
+					}
+				})
+		);
+
+		menu.addItem((item: MenuItem) =>
+			item
+				.setIcon("clipboard")
+				.setTitle("Copy Link")
+				.onClick(() => {
+					const link = event.target as HTMLAnchorElement;
+					if (link && link.href) {
+						navigator.clipboard.writeText(link.href);
+						new Notice(`Copied: ${link.href}`);
+					}
+				})
+		);
+
+		// 显示菜单
+		menu.showAtPosition({ x: event.pageX, y: event.pageY });
+	}
+
+	// handleLinkClick(event: MouseEvent) {
+	// 	const link = event.target as HTMLAnchorElement;
+	
+	// 	// 验证目标元素
+	// 	if (!link || !link.href) {
+	// 		console.error("未找到有效的链接目标" + link);
+	// 		return;
+	// 	}
+	
+	// 	// 阻止默认行为
+	// 	event.preventDefault();
+	// 	event.stopPropagation(); // 如果有其他冒泡监听器，可以尝试阻止冒泡
+	
+	// 	console.log("阻止默认的链接跳转行为");
+	// }
 	// 处理外部图片的右键菜单事件
 	eagleImageContextMenuCall(event: MouseEvent) {
+		// 将事件目标转换为HTMLImageElement类型
 		const img = event.target as HTMLImageElement;
+		// 检查图片是否在表格中
 		const inTable: boolean = img.closest('table') != null;
+		// 检查图片是否在callout中
 		const inCallout: boolean = img.closest('.callout') != null;
+		// 如果图片的ID是'af-zoomed-image'，则返回
 		if (img.id == 'af-zoomed-image') return;
+		// 如果图片的src属性不是以'http'开头，则返回
 		if (!img.src.startsWith('http')) return;
+		// 如果事件的按钮不是右键（button值不为2），则返回
 		if (event.button != 2) return;
 		// 阻止默认的右键菜单事件
 		event.preventDefault();
@@ -90,7 +176,7 @@ export default class MyPlugin extends Plugin {
 		let offset = 0;
 		if (!inPreview && (inTable || inCallout)) offset = -138;
 		menu.showAtPosition({ x: event.pageX, y: event.pageY + offset });
-		this.app.workspace.trigger("AttachFlow:contextmenu", menu);
+		//this.app.workspace.trigger("AttachFlow:contextmenu", menu);
 	}
 	registerEscapeButton(menu: Menu, document: Document = activeDocument) {
 		menu.register(
@@ -199,6 +285,39 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
+		menu.addItem((item: MenuItem) =>
+			item
+				.setIcon("external-link")
+				.setTitle("Open in windows")
+				.onClick(async () => {
+					const match = img.src.match(/\/images\/(.*)\.info/);
+					if (match && match[1]) {
+						const requestOptions: RequestInit = {
+							method: 'GET',
+							redirect: 'follow' as RequestRedirect
+						};
+
+						try {
+							const response = await fetch(`http://localhost:41595/api/item/info?id=${match[1]}`, requestOptions);
+							const result = await response.json();
+
+							if (result.status === "success" && result.data) {
+								const { id, name, ext } = result.data;
+								const infoToCopy = `ID: ${id}, Name: ${name}, Ext: ${ext}`;
+								navigator.clipboard.writeText(infoToCopy);
+								new Notice(`Copied: ${infoToCopy}`);
+							} else {
+								console.log('Failed to fetch item info');
+							}
+						} catch (error) {
+							console.log('Error fetching item info', error);
+						}
+					} else {
+						console.log('Invalid image source format');
+					}
+				})
+		);
+
 		// menu.addItem((item: MenuItem) =>
 		// 	item
 		// 		.setIcon("name")
@@ -249,6 +368,8 @@ export default class MyPlugin extends Plugin {
 				})
 		);
 	}
+
+
 }
 
 class SampleSettingTab extends PluginSettingTab {
